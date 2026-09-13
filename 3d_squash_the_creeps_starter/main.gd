ae37377b656604ce71287e8c7d7bacc0eac0edcc
Nguyen Duc Manh <izayoizero01@gmail.com>
2026-09-13 10:27:45 +0700

    Score
    Add 1 score each kill

--- 3d_squash_the_creeps_starter/main.gd
+++ 3d_squash_the_creeps_starter/main.gd
@@ -7,7 +7,7 @@ func _on_mob_timer_timeout() -> void:
 	mob_spawn_location.progress_ratio = randf()
 	mob.initialize(mob_spawn_location.position,$Player.position)
 	add_child(mob)
-
+	mob.squashed.connect($UserInterface/ScoreLabel._on_mob_squashed)
 
 func _on_player_hit() -> void:
 	$MobTimer.stop()
